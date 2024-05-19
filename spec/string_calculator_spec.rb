b870d336361f26deb3fe09ec7e6cc73c29a1b78a
rkpatel <rk.ravendra01@gmail.com>
2024-05-19 10:53:24 +0530

    test: when input contains a custom delimiter

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -27,5 +27,11 @@ RSpec.describe StringCalculator do
         expect(described_class.add("1\n2,3")).to eq(6)
       end
     end
+
+    context 'when input contains a custom delimiter using format: "//[delimiter]\n"' do
+      it 'will return sum of the numbers' do
+        expect(described_class.add("//;\n1;2")).to eq(3)
+      end
+    end
   end
 end
