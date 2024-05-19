81a318bdac2267f8892552d715fac7fb3e511c5e
rkpatel <rk.ravendra01@gmail.com>
2024-05-19 10:39:53 +0530

    Implemented handling of input contains multiple numbers separated by commas

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,8 +1,12 @@
 # frozen_string_literal: true
 
+# StringCalculator parses and adds numbers from a given input string.
 class StringCalculator
+  # Adds numbers contained in the input string and returns the sum of the numbers.
   def self.add(input)
     return 0 if input.empty?
-    input.to_i if input.size == 1
+
+    numbers = input.split(',').map(&:to_i)
+    numbers.sum
   end
 end
