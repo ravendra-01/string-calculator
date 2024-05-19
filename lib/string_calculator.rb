c7d27bb0b56d7b6c3417952d8b0bbaa750214406
rkpatel <rk.ravendra01@gmail.com>
2024-05-19 11:03:17 +0530

    Implemented handling of input contains a custom delimiter

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -2,11 +2,31 @@
 
 # StringCalculator parses and adds numbers from a given input string.
 class StringCalculator
-  # Adds numbers contained in the input string and returns the sum of the numbers.
-  def self.add(input)
-    return 0 if input.empty?
+  class << self
+    # Adds numbers contained in the input string and returns the sum of the numbers.
+    def add(input)
+      return 0 if input.empty?
 
-    numbers = input.split(/,|\n/).map(&:to_i)
-    numbers.sum
+      numbers = parse_input(input)
+      numbers.sum
+    end
+
+    private
+
+    # Returns an array of delimiters delimiters given in string using '//delim1\n'.
+    def parse_input(input)
+      if input.start_with?('//')
+        custom_delimiter, string = input.split("\n", 2)
+        custom_delimiter = custom_delimiter.gsub('//', '')
+        split_string(string, custom_delimiter)
+      else
+        split_string(input, /,|\n/)
+      end
+    end
+
+    # Splits the input string based on a delimiter and returns an array of integers.
+    def split_string(string, delim)
+      string.split(delim).map(&:to_i)
+    end
   end
 end
